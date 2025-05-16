4debaabd174f6a65df164797fbd63e7ce2639d2a
takehara-shodai <takehara.shodai@vareal.co.jp>
2025-05-17 08:24:53 +0900

    Update layout for desktop and mobile

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -18,12 +18,13 @@ function Layout() {
         />
       )}
 
-      {/* Sidebar */}
+      {/* Sidebar - Hidden on mobile, visible on desktop */}
       <div
         className={`fixed top-0 left-0 h-full z-40 transition-all duration-300 
           ${isCollapsed ? 'w-[72px]' : 'w-64'} 
           lg:translate-x-0
-          ${isMobileMenuOpen ? 'translate-x-0' : '-translate-x-full'}
+          ${isMobileMenuOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0'}
+          lg:relative
         `}
       >
         <SideNav
@@ -31,17 +32,14 @@ function Layout() {
           isCollapsed={isCollapsed}
           onCollapsedChange={setIsCollapsed}
           onToggleMode={() => {}}
-          isOpen={isMobileMenuOpen}
+          isOpen={true}
           onClose={() => setIsMobileMenuOpen(false)}
         />
       </div>
 
       {/* Main Content */}
       <div
-        className={`flex-1 min-h-screen flex flex-col transition-all duration-300
-          ${isCollapsed ? 'lg:ml-[72px]' : 'lg:ml-64'}
-          ml-0
-        `}
+        className={`flex-1 min-h-screen flex flex-col transition-all duration-300`}
       >
         <TopBar onMenuClick={() => setIsMobileMenuOpen(true)} />
         <main className="flex-1 p-4 md:p-6 bg-gray-50 dark:bg-gray-900">
